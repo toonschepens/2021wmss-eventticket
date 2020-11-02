3f177e4f028f78a2050feff0334d18ffe7718b8a
DESKTOP-NH6O2UL\toons <toon.schepens@student.odisee.be>
2020-11-02 10:03:38 +0100

    filestructure and header created

---
+++ app/public/index.php
@@ -0,0 +1,2 @@
+<?php
+require_once '../resources/layout/parts/header.php';
---
+++ app/resources/layout/parts/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/css/stylesheet.css">
+    <title>TicketSwipe</title>
+</head>
+<header class="header">
+    <button id="homeButton" class="headerButton" href= "/"> Home </button>
+    <h1 class="title"> TicketSwipe </h1>
+    <div class="search">
+        <input type="text" id="searchBox" placeholder="Eventname">
+        <button id="searchButton" class="headerButton"> Search </button>
+    </div>
+</header>
